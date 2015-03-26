c0b2dba05a2da98ca0f5c03961c6774577d317e8
Nicholas Largen <nicholaslargen@Nicholass-MacBook-Pro-2.local>
2015-03-26 10:28:03 -0500

    d2f

---
+++ BinaryTree.java
@@ -0,0 +1,65 @@
+
+public class BinaryTree
+{
+	 Node root;
+	 
+
+		public BinaryTree()
+		{
+			this.root = null;
+			 
+		}
+
+		public void displayInOrder(Node focusNode) 
+		{
+			
+			if(this.root == null)
+			{
+				System.out.println("Empty Tree");
+			}
+			else 
+			{
+				if(focusNode != null)
+				{
+					displayInOrder(focusNode.getLeftNode()); 
+					System.out.println(focusNode.getPayload());
+					displayInOrder(focusNode.getRightNode());
+				}
+					
+			
+			}
+		}
+
+		public void displayPostOrder(Node focusNode)
+		{
+			
+			if(this.root == null)
+			{
+				System.out.println("Empty Tree");
+			}
+			else
+			{
+				if(focusNode != null)
+				{
+				
+				displayPostOrder(focusNode.getLeftNode()); 
+				displayPostOrder(focusNode.getRightNode());
+				System.out.println(focusNode.getPayload());
+				}
+			}
+		}
+
+		public void add(int value)
+		{
+			Node theNode = new Node(value);
+			if(this.root == null)
+			{
+				this.root = theNode;
+			}
+			else
+			{
+				this.root.addNode(theNode);
+			}
+		}
+	
+}
---
+++ Node.java
@@ -0,0 +1,76 @@
+
+public class Node
+{
+	
+	private int payload;
+	private Node leftNode;
+	private Node rightNode;
+	
+	public Node(int payload)
+	{
+		this.payload = payload;
+		this.leftNode = null;
+		this.rightNode = null;
+	}
+
+	public void visitInorder()
+	{
+		if(this.leftNode != null)
+		{
+			this.leftNode.visitInorder();
+			
+		}
+		System.out.println(this.payload);
+		if(this.rightNode != null)
+		{
+			this.rightNode.visitInorder();
+		}
+	}
+	public void addNode(Node n)
+	{
+		if(n.getPayload() <= this.payload)
+		{
+			if(this.leftNode == null)
+			{
+				this.leftNode = n;
+			}
+			else
+			{
+				this.leftNode.addNode(n);
+			}
+		}
+		else
+		{
+			if(this.rightNode == null)
+			{
+				this.rightNode = n;
+			}
+			else
+			{
+				this.rightNode.addNode(n);
+			}
+		}
+	}
+	
+	public Node getLeftNode() {
+		return leftNode;
+	}
+
+	public void setLeftNode(Node leftNode) {
+		this.leftNode = leftNode;
+	}
+
+	public Node getRightNode() {
+		return rightNode;
+	}
+
+	public void setRightNode(Node rightNode) {
+		this.rightNode = rightNode;
+	}
+
+	public int getPayload() {
+		return payload;
+	}
+	
+
+}
